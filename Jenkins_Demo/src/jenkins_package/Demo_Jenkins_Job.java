ae0f102ece5744df62abe89a7f78f67cc6921984
Ankit_Thakur04 <Ankit_Thakur04@PUNITP339914D.ad.infosys.com>
2016-08-29 14:55:22 +0530

    Initial Chekin of Jenkins Repository

---
+++ Jenkins_Demo/src/jenkins_package/Demo_Jenkins_Job.java
@@ -0,0 +1,14 @@
+package jenkins_package;
+
+import org.testng.annotations.Test;
+
+
+public class Demo_Jenkins_Job {
+
+	@Test
+	public void testjenkins ()
+	{
+		System.out.println("Welcome to Jenkins");
+	}
+	
+}
